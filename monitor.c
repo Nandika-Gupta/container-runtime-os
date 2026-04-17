cf5d20df10c99b8658db9d1c62232594ae44809c
Nandika Gupta <nandika.g2@gmail.com>
2026-04-17 16:41:14 +0530

    Implement process registration and cleanup in monitor
    Added process tracking functionality to monitor.

--- monitor.c
+++ monitor.c
@@ -3,11 +3,12 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include "monitor_ioctl.h"
-
 #define DEVICE_NAME "container_monitor"
+#define MAX_PROCESSES 100   
 
 static int major;
-
+static int pid_list[MAX_PROCESSES];
+static int pid_count = 0;
 static int device_open(struct inode *inode, struct file *file) {
     printk(KERN_INFO "Device opened\n");
     return 0;
@@ -17,13 +18,10 @@ static int device_release(struct inode *inode, struct file *file) {
     printk(KERN_INFO "Device closed\n");
     return 0;
 }
-
 static long device_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     printk(KERN_INFO "IOCTL called\n");
-
     if (cmd == REGISTER_PROCESS) {
         struct monitor_request req;
-
         if (copy_from_user(&req, (void __user *)arg, sizeof(req))) {
             printk(KERN_INFO "copy failed\n");
             return -1;
@@ -31,11 +29,14 @@ static long device_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
         printk(KERN_INFO "Registered PID %d | soft=%dMB | hard=%dMB\n",
                req.pid, req.soft_limit, req.hard_limit);
+        if (pid_count < MAX_PROCESSES) {
+            pid_list[pid_count++] = req.pid;
+        } else {
+            printk(KERN_INFO "PID list full\n");
+        }
     }
-
     return 0;
 }
-
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = device_open,
@@ -45,19 +46,25 @@ static struct file_operations fops = {
 
 static int __init monitor_init(void) {
     major = register_chrdev(0, DEVICE_NAME, &fops);
+
+    if (major < 0) {
+        printk(KERN_ALERT "Failed to register device\n");
+        return major;
+    }
     printk(KERN_INFO "Monitor loaded with major %d\n", major);
     return 0;
 }
-
 static void __exit monitor_exit(void) {
+    printk(KERN_INFO "Cleaning up monitored processes\n");
+    for (int i = 0; i < pid_count; i++) {
+        printk(KERN_INFO "Removing PID %d from tracking\n", pid_list[i]);
+    }
 
-    printk(KERN_INFO "Cleaning up monitored processes\n");  
-
+    pid_count = 0;  // reset tracking
     unregister_chrdev(major, DEVICE_NAME);
     printk(KERN_INFO "Monitor unloaded\n");
 }
 
 module_init(monitor_init);
 module_exit(monitor_exit);
-
 MODULE_LICENSE("GPL");
